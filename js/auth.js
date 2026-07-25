1c63934b2262c056461adbfc556416fe89e82f3b
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-07-25 11:14:02 +0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,44 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth, db } from "./firebase.js";
+
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    doc,
+    setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 window.registerUser = async function () {
 
+    const username = document.getElementById("username").value;
+    const telegramId = document.getElementById("telegramId").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const referralCode = document.getElementById("referralCode").value;
 
     try {
 
-        await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
+        const userCredential =
+            await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+        const user = userCredential.user;
+
+        await setDoc(doc(db, "users", user.uid), {
+            username: username,
+            telegram_id: telegramId,
+            email: email,
+            coin: 0,
+            referral_code: username,
+            referred_by: referralCode || "",
+            active_days: 0,
+            created_at: new Date().toISOString()
+        });
 
         alert("Registration Successful");
 
@@ -22,8 +48,7 @@ window.registerUser = async function () {
 
     }
 
-}
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+};
 
 window.loginUser = async function () {
 
@@ -48,4 +73,4 @@ window.loginUser = async function () {
 
     }
 
-}
+};
