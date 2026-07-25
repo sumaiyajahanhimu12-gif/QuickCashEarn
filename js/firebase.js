d4f315e3ca566f454714131419d52f0630e80617
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-07-25 11:09:53 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -15,3 +15,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+import { auth, db } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
