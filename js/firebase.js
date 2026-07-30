636c80f5ef0b440b2aece01b81db1a0a84dd64fc
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-07-30 12:04:47 +0600

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,56 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    sendEmailVerification, 
+    signOut, 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    getDocs, 
+    collection, 
+    query, 
+    where, 
+    updateDoc, 
+    increment, 
+    addDoc, 
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLULYy8cH0O1pDqygTTgLUvmlWFEMk-9Y",
-  authDomain: "quickcashearn-2477f.firebaseapp.com",
-  projectId: "quickcashearn-2477f",
-  storageBucket: "quickcashearn-2477f.firebasestorage.app",
-  messagingSenderId: "573761874377",
-  appId: "1:573761874377:web:3aa98c3062bcd9aff1a018"
+    apiKey: "AIzaSyBLULYy8cH0O1pCqygTTgLUvmlWFEMk-9Y",
+    authDomain: "quickcashearn-2477f.firebaseapp.com",
+    projectId: "quickcashearn-2477f",
+    storageBucket: "quickcashearn-2477f.firebasestorage.app",
+    messagingSenderId: "573761874377",
+    appId: "1:573761874377:web:3aa98c3062bcd9aff1a018",
+    measurementId: "G-X7TD58F2K"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendEmailVerification,
+    signOut,
+    onAuthStateChanged,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    collection,
+    query,
+    where,
+    updateDoc,
+    increment,
+    addDoc,
+    serverTimestamp
+};
